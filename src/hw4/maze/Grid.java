17c0cd6315bed39db7277155d2ad7ff02c53e36b
steinmeyerspencer <spencersteinmeyer@gmail.com>
2025-04-17 10:38:37 -0500

    Set up most of what I think is necessary for Grid, Cell, Row.

---
+++ src/hw4/maze/Grid.java
@@ -0,0 +1,28 @@
+package hw4.maze;
+
+import java.util.ArrayList;
+
+public class Grid {
+	private ArrayList<Row> rows;
+
+	public Grid(ArrayList<Row> rows) {
+		this.rows = rows;
+	}
+
+	public ArrayList<Row> getRows() {
+		return rows;
+	}
+
+	public void setRows(ArrayList<Row> rows) {
+		this.rows = rows;
+	}
+
+	@Override
+	public String toString() {
+		return "Grid [rows=" + rows + "]";
+	}
+	
+	
+	
+
+}
---
+++ src/hw4/maze/Row.java
@@ -0,0 +1,19 @@
+package hw4.maze;
+
+import java.util.ArrayList;
+
+public class Row {
+	
+	private ArrayList<Cell> rowVals;
+	// might need to change this because we won't have getters or setters
+
+	public Row(ArrayList<Cell> rowVals) {
+		this.rowVals = rowVals;
+	}
+	
+	
+	
+	
+	
+	
+}
