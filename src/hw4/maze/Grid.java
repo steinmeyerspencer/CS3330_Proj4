7b62fa7694cedc665507eab654b49f5093552170
steinmeyerspencer <spencersteinmeyer@gmail.com>
2025-04-17 13:13:58 -0500

    GridTest passes, added javadocs to Grid

--- src/hw4/maze/Grid.java
+++ src/hw4/maze/Grid.java
@@ -5,18 +5,33 @@ import java.util.ArrayList;
 public class Grid {
 	private ArrayList<Row> rows;
 
+	/**
+	 * parameterized constructor. takes arrayList of rows and makes into a grid
+	 * @param rows
+	 */
 	public Grid(ArrayList<Row> rows) {
 		this.rows = rows;
 	}
 
+	/**
+	 * returns rows
+	 * @return
+	 */
 	public ArrayList<Row> getRows() {
 		return rows;
 	}
 
+	/**
+	 * sets rows, parameter is an arrayList<Rows>
+	 * @param rows
+	 */
 	public void setRows(ArrayList<Row> rows) {
 		this.rows = rows;
 	}
 
+	/**
+	 * simple toString()
+	 */
 	@Override
 	public String toString() {
 		return "Grid [rows=" + rows + "]";
