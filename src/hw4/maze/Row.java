6346c210f7f3e0a4c98a506ad60cb5353bade07d
steinmeyerspencer <spencersteinmeyer@gmail.com>
2025-04-17 10:40:24 -0500

    Removed all compile errors from CellTest, GridTest, RowTest.

--- src/hw4/maze/Row.java
+++ src/hw4/maze/Row.java
@@ -4,12 +4,24 @@ import java.util.ArrayList;
 
 public class Row {
 	
-	private ArrayList<Cell> rowVals;
+	private ArrayList<Cell> cells;
 	// might need to change this because we won't have getters or setters
 
-	public Row(ArrayList<Cell> rowVals) {
-		this.rowVals = rowVals;
+	public Row(ArrayList<Cell> cells) {
+		this.cells = cells;
 	}
+
+	public ArrayList<Cell> getCells() {
+		return cells;
+	}
+
+	public void setCells(ArrayList<Cell> cells) {
+		this.cells = cells;
+	}
+	
+	
+	
+	
 	
 	
 	
